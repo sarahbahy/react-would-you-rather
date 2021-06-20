1f3a614b9311e5d37ee0a16801d1c220c679dc4b
sarahbahi <sarahbahyy@gmail.com>
2021-06-21 00:19:30 +0200

    browser roouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch,Redirect } from 'react-router-dom'
 import Login from '../components/Login'
 import { connect } from 'react-redux'
 import { handleInitialUsers } from '../actions/shared'
@@ -15,26 +15,30 @@ class App extends Component {
     const AUTHED_ID = null;
     this.props.dispatch((handleInitialUsers(AUTHED_ID)))
   }
-
+  // function Greeting(props) {
+  //   const isLoggedIn = props.isLoggedIn;
+  //   if (isLoggedIn) {
+  //     return <UserGreeting />;
+  //   }
+  //   return <GuestGreeting />;
+  // }
+  // <Greeting isLoggedIn={false} />,
   render() {
     const { authedUser } = this.props
     return (
       <Router>
         <Fragment>
           <LoadingBar style={{ backgroundColor: '#25baa2'}}/>
-          <Switch>
-            { 
-              authedUser === null
-              ? <Route path='/' exact component={Login} />
-              : <Fragment>
-                  <Route path='/' exact component={Dashboard} />
-                  <Route path='/questions/:question_id' component={questionDetails} />
-                  <Route path='/add' exact component={addquestion} />
-                  <Route path='/leaderboard' exact component={Leaderboard} />
-                </Fragment>
-            }
-            <Route component={PageNotFound} />
-          </Switch>
+            
+            <Switch>
+            <Route path='/' exact component={ authedUser === null ? Login:Dashboard} />
+            {/* <Route   component={ Dashboard} /> */}
+            <Route path='/questions/:question_id' component={ questionDetails} />
+            <Route path='/add' exact component={addquestion } />
+            <Route path='/leaderboard' exact component={ Leaderboard } />
+            <Route path='*' component={PageNotFound} />
+            </Switch>
+            
         </Fragment>
       </Router>
 
